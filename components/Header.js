271067499f7cc6dc3a4f9192c3673f75d225d9ab
Caleb Crosby Adainoo <code.crosby@gmail.com>
2021-06-04 22:11:50 +0000

    feat: added and styled the header item component

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,32 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+	BadgeCheckIcon,
+	CollectionIcon,
+	HomeIcon,
+	LightningBoltIcon,
+	SearchIcon,
+	UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
 	return (
 		<header>
-			<h1>Header</h1>
-			<Image src="https://links.papareact.com/ua6" width={200} height={100} />
+			<div className="flex flex-grow justify-evenly max-w-2xl">
+				<HeaderItem title="HOME" Icon={HomeIcon} />
+				<HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+				<HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+				<HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+				<HeaderItem title="SEARCH" Icon={SearchIcon} />
+				<HeaderItem title="ACCOUNT" Icon={UserIcon} />
+			</div>
+
+			<Image
+				className="object-contain"
+				src="https://links.papareact.com/ua6"
+				width={200}
+				height={100}
+			/>
 		</header>
 	);
 }
